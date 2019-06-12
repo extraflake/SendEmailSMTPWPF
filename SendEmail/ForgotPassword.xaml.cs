d06aa9b770059cd6a9ede2f43d5313293b74494f
Naufal Aji Wibowo <naufal.aji@mii.co.id>
2019-06-12 08:54:04 +0700

    adding ForgotPassword

---
+++ SendEmail/ForgotPassword.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SendEmail
+{
+    /// <summary>
+    /// Interaction logic for ForgotPassword.xaml
+    /// </summary>
+    public partial class ForgotPassword : Window
+    {
+        public ForgotPassword()
+        {
+            InitializeComponent();
+        }
+
+        private void Cancel_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            Login calling = new Login();
+            calling.Show();
+        }
+
+        private void Check_Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
